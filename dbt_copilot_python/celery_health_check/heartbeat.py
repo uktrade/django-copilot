16c2d0e12f8f8885c7db5388f85943d52ae9dcaf
Samuele Mattiuzzo <samuele.mattiuzzo@digital.trade.gov.uk>
2024-01-25 15:56:18 +0000

    Cleanup of Heartbeat

--- dbt_copilot_python/celery_health_check/heartbeat.py
+++ dbt_copilot_python/celery_health_check/heartbeat.py
@@ -1,7 +1,7 @@
+from datetime import UTC
 from datetime import datetime
 
 from celery import bootsteps
-from dateutil.tz import tz
 
 from .const import HEARTBEAT_FILE
 
@@ -10,13 +10,11 @@ class HeartBeat(bootsteps.StartStopStep):
     requires = {"celery.worker.components:Timer"}
 
     def __init__(self, parent, **kwargs):
-        print("LivenessProbe.__init__")
         super().__init__(parent, **kwargs)
         self.requests = []
         self.tref = None
 
     def start(self, worker):
-        print("LivenessProbe.start")
         self.tref = worker.timer.call_repeatedly(
             1.0,
             self.update_heartbeat_file,
@@ -25,11 +23,7 @@ class HeartBeat(bootsteps.StartStopStep):
         )
 
     def stop(self, worker):
-        print("LivenessProbe.stop")
         HEARTBEAT_FILE.unlink(missing_ok=True)
 
     def update_heartbeat_file(self, worker):
-        print("LivenessProbe.update_heartbeat_file")
-        print(f"HEARTBEAT_FILE: {HEARTBEAT_FILE}")
-        HEARTBEAT_FILE.write_text(str(datetime.timestamp(datetime.now(tz=tz.UTC))))
-        print(f"HEARTBEAT_FILE contents after update: {HEARTBEAT_FILE.read_text()}")
+        HEARTBEAT_FILE.write_text(str(datetime.timestamp(datetime.now(UTC))))
